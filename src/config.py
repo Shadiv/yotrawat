1738a9def2ee299e9f29026a1ec331e22b69871e
Shadiv <wisenheim@mail.ru>
2023-04-24 17:13:12 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,10 +1,22 @@
+from telebot.asyncio_handler_backends import State, StatesGroup
+
 TOKEN='token'
 COMMANDS = ['start']
 CHAT_WORD_COMMANDS = ['Программа']
-days = {"понедельник": 0,
-        "вторник": 1,
-        "среда": 2,
-        "четверг": 3,
-        "пятница": 4,
-        "суббота": 5,
-        "воскресенье": 6}
+# days = {"понедельник": 0,
+#         "вторник": 1,
+#         "среда": 2,
+#         "четверг": 3,
+#         "пятница": 4,
+#         "суббота": 5,
+#         "воскресенье": 6}
+
+class UserStates(StatesGroup):
+    user_id = State()
+    gender = State()
+    schedule = State()
+
+# set_state -> sets a new state
+# delete_state -> delets state if exists
+# get_state -> returns state if exists
+
